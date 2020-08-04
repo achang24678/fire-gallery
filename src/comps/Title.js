d1a176b4ec61abf0784fbc5203b9ad7c5ed5d8ee
Allen Chang <achang24678@gmail.com>
2020-08-04 01:13:59 -0700

    s

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Title = () => {
   return (
     <div className="title">
       <h1>FireGram</h1>
       <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elits.</p>
     </div>
-  )
-}
+  );
+};
 
 export default Title;
